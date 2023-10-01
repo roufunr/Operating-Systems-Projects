bf5a16b2f64787120ac97fef5a3f81e377d0a8ee
roufunr <rouf@nevada.unr.edu>
2023-10-01 12:38:15 -0700

    changed in plain sum

--- pThread/random_number_generator/plain_sum.py
+++ pThread/random_number_generator/plain_sum.py
@@ -1,7 +1,7 @@
 total_sum = 0
 
 # Open the file for reading
-with open('./../PA2_Rouf_Abdur/oneMillion.txt', 'r') as file:
+with open('./../PA2_Rouf_Abdur/oneThousand.txt', 'r') as file:
     for line in file:
         # Convert the line to an integer and add it to the total sum
         total_sum += int(line)
