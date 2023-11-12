faaa99c0522735bb5272bd344b539185d436138c
roufunr <rouf@nevada.unr.edu>
2023-11-12 12:46:43 -0800

    mymalloc implementation has been started

---
+++ dynamic_memory_allocation/code/mymalloc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int main() {
+    printf("Hello world\n");
+    return 0;
+}
